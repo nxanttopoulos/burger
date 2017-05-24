ef76f42b939786be4c8b44ff13bb9e2ad265e8ea
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-05-24 16:36:48 -0400

    forgot to push updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,46 +1,34 @@
+//Requiring the models
 var express = require("express");
 var router = express.Router();
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+var db = require("../models");
+//Routing
+  //Get burgers
+  router.get("/", function(req, res){
+    db.burgers.findAll({}).then(function(data){
+      var handlebarsObject = {
+        burgers: data
+      };
+      res.render("index", handlebarsObject);
+    });
+  });
+  //Create new burgers
+  router.post("/", function(req, res){
+    db.burgers.create({
+      burger_name: req.body.name,
+    }).then(function(){
+      res.redirect("/");
+    });
+  });
+  router.put("/:burger_name", function(req, res){
+    db.burgers.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        burger_name: req.params.burger_name
+      }
+    }).then(function(){
+      res.redirect("/");
   });
 });
-
-router.post("/", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function() {
-    res.redirect("/");
-  });
-});
-
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
-});
-
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function() {
-    res.redirect("/");
-  });
-});
-// Export routes for server.js to use.
-module.exports = router;
+module.exports=router;
