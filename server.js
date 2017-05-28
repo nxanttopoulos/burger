feffa16beea146b2142faa04c850cbf13bc7cb3e
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-05-28 18:38:26 -0400

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
